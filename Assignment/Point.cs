126049d8d73b7d5d130a08d9436715ddee79012e
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2024-12-22 02:01:26 +0200

    Assignment

---
+++ Assignment/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Point
+    {
+        public int x; public int y;
+        public Point(int _x, int _y)
+        {
+            x = _x; y = _y;
+        }
+    }
+}
